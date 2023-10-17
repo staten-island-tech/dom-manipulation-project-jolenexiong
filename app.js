ed0558aadf1d50be6dd76cedf8bab375b75f8747
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2023-10-17 13:14:31 -0400

    nothing

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-const form = document.querySelector ("#form");
-const info =document.querySelector (".info")
+
+DOMSelectors = {
+    form: document.querySelector("#form"),
+    college: document.querySelector(".college"),
+    h3s: document.querySelectorAll("h3s"),
+};
+
+console.log(DOMSelectors.#form);
+
+DOMselectors.form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log(DOMSelectors.college.value);
+    DomSelectors.h3s, forEach(
+        (h3s) => h3s.textContent = DOMSelectors.college.value)
+        
+});
