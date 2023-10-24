0b9dd7b9e0144df9ef13fc9f64d2eae471cf4581
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2023-10-24 13:16:02 -0400

    d

--- dom.js
+++ dom.js
@@ -1,20 +1,33 @@
 const DOMSelectors = {
     form: document.querySelector("#form"),
-    name: document.querySelector(".name"),
+    album: document.querySelector("#album"),
+    artist: document.querySelector("#artist"),
+    picture: document.querySelector("#picture"),
     h2s: document.querySelectorAll("h2s"),
+    //add gallery
 };
-
-DOMselectors.form.addEventListener("submit", function (event) {
-    event.preventDefault();
-   
-});
-
-function insertcard () {
-    document.querySelector(".gallery").insertAdjacentHTML
+function album(){
+    //write code here to get values
+}
+function injectcard (form) {
+    DOMSelectors.gallery.insertAdjacentHTML
     ("afterbegin",
         `<div class="card">
-            <h2 class="card.title">${DOMSelectors.name.value}</h2>
+            <h2 class="card.title">${form.album.value}</h2>
+            <h2 class="aritst">${form.artist.value}</h2>
+            <h2 class="picture">${form.artist.value}</h2>
         </div>`
-
     );
 }
+
+DOMSelectors.form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    //need make album first
+   injectcard() //function dude, need ()
+   //clear fields
+   //remove buttons
+});
+
+//we can test injectcard here
+//get working
+injectcard({album:"test",artist:"shdfhjikl"})
